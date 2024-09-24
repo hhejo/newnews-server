ec0459dca4f0c376fa2595e1a9c5e5d718b89d79
hhejo <litsmainjof@gmail.com>
2024-09-24 11:07:51 +0900

    feat: add search route, controller

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import booksRouter from './routes/books.js';
+import searchRouter from './routes/search.js';
 
 const app = express();
 const port = 3000;
@@ -8,5 +9,6 @@ const port = 3000;
 app.use(cors());
 
 app.use('/api/books', booksRouter);
+app.use('/api/search', searchRouter);
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
