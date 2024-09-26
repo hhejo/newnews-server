2cc6bc60c6b1780fe102e43205bd47e65be7e9ad
hhejo <litsmainjof@gmail.com>
2024-09-26 15:37:36 +0900

    feat: update request url

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,44 +1,41 @@
 import axios from 'axios';
 import { TTB_KEY } from '../env.mjs';
+import { getBooksFromData } from './books-functions.js';
 
 const TIME_DIFF = 600_000; // 10분
 const lastUpdateTime = Date.now();
-let cacheBooks = [];
-let cacheBook = {};
+let cachedBooks = [];
+let cachedBook = {};
 
+const baseUrl = 'http://www.aladin.co.kr/ttb/api';
 const baseParams = { ttbkey: TTB_KEY, output: 'js', version: 20131101 };
 
-export const getBooksNew = async (req, res) => {
-  const url = 'http://www.aladin.co.kr/ttb/api/ItemList.aspx';
-  const params = {
-    ...baseParams,
-    queryType: 'ItemNewAll',
-    maxResults: 10,
-    searchTarget: 'Book',
-    cover: 'Big',
-  };
+export const getBooks = async (req, res) => {
+  if (lastUpdateTime + TIME_DIFF > Date.now() && cachedBooks.length > 0)
+    return res.json(cachedBooks);
 
-  if (
-    lastUpdateTime + TIME_DIFF > Date.now() &&
-    Object.keys(cacheBooks).length > 0
-  ) {
-    console.log('Cached books');
-    return res.json(cacheBooks);
-  }
-
+  const url = `${baseUrl}/ItemList.aspx`;
+  const params = { ...baseParams, ...req.query, searchTarget: 'Book' };
   try {
     const { data } = await axios.get(url, { params });
-    cacheBooks = data;
-    console.log('Fetched books');
-    return res.json(data);
+    const books = getBooksFromData(data);
+    cachedBooks = books;
+    return res.json(books);
   } catch (err) {
     console.error('Error:', err);
   }
 };
 
 export const getBook = async (req, res) => {
-  const url = 'http://www.aladin.co.kr/ttb/api/ItemLookUp.aspx';
+  if (
+    lastUpdateTime + TIME_DIFF > Date.now() &&
+    Object.keys(cachedBook).length > 0
+  )
+    return res.json(cachedBook);
+
+  const url = `${baseUrl}/ItemLookUp.aspx`;
   const { isbn13 } = req.params;
+  // const params = { ...baseParams, itemIdType: 'ISBN', itemId: isbn13, cover: 'big' };
   const params = {
     ...baseParams,
     itemIdType: 'ISBN',
@@ -46,17 +43,9 @@ export const getBook = async (req, res) => {
     cover: 'big',
   };
 
-  if (
-    lastUpdateTime + TIME_DIFF > Date.now() &&
-    Object.keys(cacheBook).length > 0
-  ) {
-    console.log('Cached book');
-    return res.json(cacheBook);
-  }
-
   try {
     const { data } = await axios.get(url, { params });
-    cacheBook = data;
+    cachedBook = data;
     console.log('Fetched book');
     return res.json(data);
   } catch (err) {
