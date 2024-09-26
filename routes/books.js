4d2acabdce2ec3f6f565cb0414684e023c1d2862
hhejo <litsmainjof@gmail.com>
2024-09-27 06:22:07 +0900

    feat: update routes path

--- routes/books.js
+++ routes/books.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getBooks, getBook } from '../controllers/books.controller.js';
+import { getBooks, getBook } from '../controllers/books/books.controller.js';
 
 const router = Router();
 
