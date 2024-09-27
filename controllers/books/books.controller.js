448a61a6507acb755cfe4377c0b2e9be3dc285dd
hhejo <litsmainjof@gmail.com>
2024-09-27 10:35:46 +0900

    feat: update query string

--- controllers/books/books.controller.js
+++ controllers/books/books.controller.js
@@ -22,7 +22,8 @@ export const getBooks = cachingDecorator(async (req, res) => {
 
 export const getBook = cachingDecorator(async (req, res) => {
   const url = `${baseUrl}/ItemLookUp.aspx`; // http://www.aladin.co.kr/ttb/api/ItemList.aspx
-  const params = { ...baseParams, ...req.query, itemId: req.params.isbn13 }; // ?ttbkey=TTBKEY&output=js&version=20131101&itemIdType=ISBN&cover=big&itemId=ITEMID
+  const itemId = req.params.isbn13;
+  const params = { ...baseParams, ...req.query, itemId, itemIdType: 'ISBN' }; // ?ttbkey=TTBKEY&output=js&version=20131101&itemIdType=ISBN&cover=big&itemId=ITEMID
   try {
     const { data } = await axios.get(url, { params, timeout: 10_000 });
     const book = getBookFromData(data);
