7c8602f01d55b2e384690c2f0ceacf61dfd9bacf
hhejo <litsmainjof@gmail.com>
2024-09-28 18:57:24 +0900

    feat: update url

--- controllers/search/search.controller.js
+++ controllers/search/search.controller.js
@@ -4,7 +4,7 @@ import { cachingDecorator, getBooksFromData } from '../../utils/utils.js';
 
 export const searchBook = cachingDecorator(async (req, res) => {
   const url = `${baseUrl}/ItemSearch.aspx`; // http://www.aladin.co.kr/ttb/api/ItemSearch.aspx
-  const params = { ...baseParams, ...req.query }; // ?ttbkey=TTBKEY&output=js&version=20131101&queryType=ItemNewAll&cover=Big&maxResults=10&searchTarget=Book
+  const params = { ...baseParams, ...req.query }; // ?ttbkey=TTBKEY&output=js&version=20131101&query=QUERY&queryType=QUERYTYPE&cover=Big&maxResults=10&searchTarget=Book
   try {
     const { data } = await axios.get(url, { params, timeout: 10_000 });
     const books = getBooksFromData(data);
